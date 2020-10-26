4074e2f980c27a16c941a8f65ed194b6670ce9a6
aucelio <aucelio.barbosa@gmail.com>
2020-10-26 19:53:55 -0300

    app com problema com relacionamento com main.xml no ubuntu-20.04

---
+++ app/src/main/java/com/example/meu_play/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.meu_play
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.media.MediaPlayer
+class MainActivity : AppCompatActivity() {
+
+    private var mediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.WishYoudComeTrue)
+
+        btn_play.setOnClickListener(mediaPlayer?.start())
+        btn_pause.setOnClickListener(mediaPlayer?.pause())
+        btn_stop.setOnClickListener(mediaPlayer?.stop())
+
+    }
+}
